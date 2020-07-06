216eb4622b3b8f1c65bb2b38eb5994c6f45ba4e4
rahul harlalka <rahulharlalka96@gmail.com>
2020-07-06 12:05:48 +0530

    utils files issue solved

--- cvlib/utils.py
+++ cvlib/utils.py
@@ -24,7 +24,7 @@ def download_file(url,file_name,dest_dir):
     chunk_size=1024
     num_bars=round(file_size/chunk_size)
 
-    bar=pb.ProgressBar(maxval=num_bars).start()
+    #bar=pb.ProgressBar(maxval=num_bars).start()
     #pylint: disable=maybe-no-member
     if r.status_code != requests.codes.ok :
         print("error occured while downlading")
@@ -34,7 +34,7 @@ def download_file(url,file_name,dest_dir):
     with open(full_path_to_file,'wb') as file:
         for chunk in r.iter_content(chunk_size=chunk_size):
             file.write(chunk)
-            bar.update(chunk)
+            #bar.update(chunk)
             count+=1
 
     return full_path_to_file
